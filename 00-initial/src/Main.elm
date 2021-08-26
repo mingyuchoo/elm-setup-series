ea18d4beb498c304e811303d82a322b0c1b4d3c0
mingyuchoo <mingyuchoo@gmail.com>
2021-08-27 04:37:12 +0900

    macbookpro

--- 00-initial/src/Main.elm
+++ 00-initial/src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (..)
 
+--------------------------------------------------------------------------------
 -- Press buttons to increment and decrement a counter.
 --
 -- Read how it works:
@@ -7,58 +8,43 @@ module Main exposing (..)
 --
 
 
+--------------------------------------------------------------------------------
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
-
-
+--------------------------------------------------------------------------------
 -- MAIN
 
+main = Browser.sandbox { init = init, update = update, view = view }
 
-main =
-  Browser.sandbox { init = init, update = update, view = view }
-
-
-
+--------------------------------------------------------------------------------
 -- MODEL
-
 
 type alias Model = Int
 
 
 init : Model
-init =
-  0
+init = 0
 
-
-
+--------------------------------------------------------------------------------
 -- UPDATE
 
-
-type Msg
-  = Increment
-  | Decrement
+type Msg = Increment
+         | Decrement
 
 
 update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
+update msg model = case msg of
+                     Increment -> model + 1
+                     Decrement -> model - 1
 
-    Decrement ->
-      model - 1
-
-
-
+--------------------------------------------------------------------------------
 -- VIEW
 
-
 view : Model -> Html Msg
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+view model = div [] [ text   "Hello World!"
+                    , button [ onClick Decrement ] [ text "-" ]
+                    , div    [] [ text (String.fromInt model) ]
+                    , button [ onClick Increment ] [ text "+" ]
+                    ]
--- README.md
+++ README.md
@@ -4,21 +4,48 @@
 
 - <https://guide.elm-lang.org/install/elm.html>
 
-## Basic commnads
+### Create a project
 
 ```bash
+mkdir <project-name>
+
+cd <project-name>
+
 elm init
+```
 
-elm reactor
+### Install modules
 
-elm make src/Main.elm
-
-elm make src/Main.elm --optimize --output=elm.js
-
+```bash
 elm install elm/http
 
 elm install elm/json
 ```
+
+## How to build
+
+### Run Elm Dashboard at `http://localhost:8000`
+
+```bash
+elm reactor
+```
+
+### Generate `index.html` file from `Main`
+
+```bash
+elm make src/Main.elm
+```
+
+### Generate `elm.js` file from `Main`
+
+```bash
+elm make src/Main.elm --optimize --output=elm.js
+```
+
+## How to run
+
+open `index.html` file with a `web browser`.
+
 
 ## References
 
